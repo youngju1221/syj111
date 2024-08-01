fb66dec1ff41d6fdec5bf7ac230a42e536557f8f
WIN-PJHG9NNOGAB\ict-26 <oasis8658@naver.com>
2024-08-01 09:14:14 +0900

    no message

---
+++ Java_workspace/JAVA_workshop02/src/workshop02/Test02.java
@@ -0,0 +1,15 @@
+package workshop02;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		 for (int inx = 0; inx <= 6; inx++) {
+		            
+		     for (int jnx = 0; jnx <= inx; jnx++) {
+		                System.out.print("*");
+		     }
+		     // 출력
+		     System.out.print("@");
+		 }
+	}//END OF MAIN
+}
